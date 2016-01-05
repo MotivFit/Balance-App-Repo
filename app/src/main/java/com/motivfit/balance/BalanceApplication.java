9db3580df76de284da19151ac75beecd2f5b8512
pinpong <o.nitzschke@gmail.com>
2016-01-05 22:27:59 +0100

    Add Application class

---
+++ app/src/main/java/com/motivfit/balance/BalanceApplication.java
@@ -0,0 +1,15 @@
+package com.motivfit.balance;
+
+import android.app.Application;
+
+/**
+ * Created by pinpong on 05.01.16.
+ */
+public class BalanceApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //TODO So, we can initialize some stuff in background thread if needed
+    }
+}
